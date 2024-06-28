23154aeadaae02423ccea96c9ebcce04e2f26d2a
maikynata <maikynata25@gmail.com>
2024-06-28 17:05:43 +0200

    first commit

---
+++ Day 1 - Data Types/data-types.js
@@ -0,0 +1,19 @@
+// Declare second integer, double, and String variables.
+var x;
+var y;
+var z;
+
+// Read and save an integer, double, and String to your variables.
+x = parseInt(readLine());
+y = parseFloat(readLine());
+z = readLine();
+
+// Print the sum of both integer variables on a new line.
+console.log(x+i);
+// Print the sum of the double variables on a new line.
+dob = y+d;
+console.log(dob.toFixed(1));
+
+// Concatenate and print the String variables on a new line
+// The 's' variable above should be printed first.
+console.log(s + z);
---
+++ README.md
@@ -0,0 +1,2 @@
+## HackerRank 30 days of Coding Challanges to practice
+## This is a repo with JavaScript solutions, also look the Python repo answers here.
